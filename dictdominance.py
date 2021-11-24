c49f7bbdf514cf583086a1f49cddc22513c3da07
Naga Teja <teja@pc-1-116.customer.ask4.lan>
2021-11-24 15:29:28 +0000

    did dictdominance

--- dictdominance.py
+++ dictdominance.py
@@ -8,6 +8,49 @@
 # • dict1={‘a’:1,‘b’:1} and dict2={‘c’:0}. In this case, dict1 is not greater than dict2, as dict1 does not have the key c; therefore the function should return False.
 
 
+# if even one (key,value) in dict1 is greater than  (key,value) in dict2:
+# 	  then return True
+
+# if all the key,value in dict1 and dict2 are equal :
+# 	 then return False
+ 
+# if there is a similar key in dict1 and dict2 :
+# 	and values of the simliar keys of dict1 are greater than dict2 :
+# 		then return True
+# 	else if there are no similar pairs:
+# 		 then return False
+
 
 def isGreaterThan(dict1,dict2):
-	
+	status = False
+
+
+	similar_key_dict1 = dict()
+	similar_key_dict2 = dict()
+
+	for i in dict1.keys():
+		for j in dict2.keys():
+			if i == j :
+				similar_key_dict1[i] = dict1[i]
+				similar_key_dict2[j] = dict2[j]
+
+
+	if bool(similar_key_dict1) == False and bool(similar_key_dict2) == False:
+		status = False
+
+
+
+
+	for k in similar_key_dict1.values():
+		for n in similar_key_dict2.values():
+			if k > n :
+				status = True
+
+
+
+	return status
+			
+
+
+print(isGreaterThan({'a':1,'b':0, 'c': 0},{'a':0,'b':1}))
+# print(isGreaterThan({'a':1,'b':0},{'c': 0}))
--- rough.py
+++ rough.py
@@ -1,4 +1,25 @@
+	# status = False
 
+	# keys_dict1 = [ i for i in dict1.keys()]
+	# keys_dict2 = [j for j in dict2.keys()]
+	# values_dict1 = [n for n in dict1.values()]
+	# values_dict2 = [m for m in dict2.values()]
+
+	# for key_1 in keys_dict1:
+	# 	for key_2 in keys_dict2:
+	# 		if key_1 == key_2:
+	# 			status = True
+	# 			if values_dict1[keys_dict1.index(key_1)] > values_dict2[keys_dict2.index(key_2)]:
+	# 				status = True
+	# 			elif values_dict1[keys_dict1.index(key_1)] == values_dict2[keys_dict2.index(key_2)]:
+	# 				status = False
+	# 		check = any(item in keys_dict1 for item in keys_dict2)
+	# 		elif key_1 != key_2 and check =True:
+	# 			status = True
+				
+
+
+	# return status
 
 stack = [[]]
 
